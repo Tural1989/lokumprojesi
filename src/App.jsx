30c874d0fb96144757c1edfa517d28021b062531
Tural1989 <82092532+Tural1989@users.noreply.github.com>
2024-12-07 00:27:50 +0400

    hosgeldiniz yazisi eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <p>HOSGELDINIZ!</p>
         <p>
          Git ve github ogreniyorum/tofi ve tural
-        
         </p>
       </div>
       <p className="read-the-docs">
