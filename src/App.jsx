b722b476ff3254b325f6f2b64bb93bcbc2462db3
Tural1989 <82092532+Tural1989@users.noreply.github.com>
2024-12-07 00:55:06 +0400

    yeni degisiklik

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <p>HOSGELDINIZ!</p>
+        <p>HOSGELDINIZ!..</p>
         <p>
          Git ve github ogreniyorum/tofi ve tural
         </p>
